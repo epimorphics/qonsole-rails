47d68d90ec3434ee07e722ce1e4ca30f11f03a53
Ian Dickinson <ian@epimorphics.com>
2014-03-11 17:26:23 +0000

    Showing pre-defined prefixes

--- app/helpers/qonsole_rails/qonsole_helper.rb
+++ app/helpers/qonsole_rails/qonsole_helper.rb
@@ -4,17 +4,29 @@ module QonsoleRails
     def render_examples( config )
       capture do
         config.queries.each do |example_query|
-          concat(
-            content_tag( "li" ) do
-              tag( "input", {class: 'btn',
-                             type: 'button',
-                             value: example_query[:name],
-                             'data-query' => example_query[:query]
-                            } )
-            end
-          )
+          render_button( example_query[:name], 'data-query' => example_query[:query] )
         end
       end
+    end
+
+    def render_prefixes( config )
+      capture do
+        config.prefixes.each do |prefix, uri|
+          render_button( prefix, 'data-uri' => uri )
+        end
+      end
+    end
+
+    def render_button( value, options )
+      concat(
+        content_tag( "li" ) do
+          tag( "input", {class: 'btn',
+                         type: 'button',
+                         value: value
+                        }.merge( options )
+             )
+        end
+      )
     end
   end
 end
