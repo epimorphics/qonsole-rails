08c79d5e145e9d88f25e78f85a170cc212d623bc
Ian Dickinson <ian@epimorphics.com>
2016-05-31 11:24:45 +0100

    Load jquery datatables into asset pipeline

--- app/assets/javascripts/qonsole_rails/application.js
+++ app/assets/javascripts/qonsole_rails/application.js
@@ -8,5 +8,7 @@
 //= require codemirror/modes/turtle
 //= require codemirror/modes/xml
 //= require codemirror/modes/javascript
+//= require dataTables/jquery.dataTables
+//= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
 
 //= require_tree .
