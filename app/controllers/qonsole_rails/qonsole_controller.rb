0f396d573ca82a056b888248e9fbbdf5a6d77d7e
Ian Dickinson <i.j.dickinson@gmail.com>
2017-03-17 18:16:41 +0000

    Resolve Rubocop warnings

--- app/controllers/qonsole_rails/qonsole_controller.rb
+++ app/controllers/qonsole_rails/qonsole_controller.rb
@@ -1,60 +1,66 @@
 require_dependency "qonsole_rails/application_controller"
 
 module QonsoleRails
+  # Controller for Qonsole Rails engine
   class QonsoleController < ApplicationController
     layout "application"
 
     def index
-      @qconfig = QonsoleConfig.new( params, hostname )
+      @qconfig = QonsoleConfig.new(params, hostname)
     end
 
     def create
-      qonfig = QonsoleConfig.new( params, hostname )
+      qonfig = QonsoleConfig.new(params, hostname)
 
       if qonfig.valid_endpoint?
-        query_service = QonsoleRails::SparqlQueryService.new( qonfig )
+        query_service = QonsoleRails::SparqlQueryService.new(qonfig)
         render json: query_service.run, layout: false
       else
-        render text: "You do not have access to the given SPARQL endpoint", status: :forbidden, layout: false
+        render(text: "You do not have access to the given SPARQL endpoint",
+               status: :forbidden,
+               layout: false)
       end
     end
 
     unless Rails.application.config.consider_all_requests_local
-      rescue_from ActionController::RoutingError, :with => :render_404
+      rescue_from ActionController::RoutingError, with: :render_404
       rescue_from Exception, with: :render_exception
     end
 
-    def render_exception( e )
-      if e.instance_of?( ArgumentError ) || e.instance_of?( RuntimeError )
-        render_error( 400, e )
+    def render_exception(e)
+      if e.instance_of?(ArgumentError) || e.instance_of?(RuntimeError)
+        render_error(400, e)
       elsif e.instance_of? ActionController::InvalidAuthenticityToken
         Rails.logger.warn "Invalid authenticity token #{e}"
-        render_error( 403, e )
+        render_error(403, e)
       else
         Rails.logger.warn "No explicit error page for exception #{e} - #{e.class.name}"
-        render_error( 500, e )
+        render_error(500, e)
       end
     end
 
-    def render_404( e = nil )
-      render_error( 404, e )
+    def render_404(e = nil)
+      render_error(404, e)
     end
 
-    def render_error( status, e )
+    def render_error(status, e)
       respond_to do |format|
-        format.html { render( layout: false,
-                              file: Rails.root.join( 'public', 'landing', status.to_s ),
-                              status: status ) }
-        format.all { render text: e ? "#{e}" : status.to_s, status: status }
+        format.html do
+          render(layout: false,
+                 file: Rails.root.join('public', 'landing', status.to_s),
+                 status: status)
+        end
+
+        format.all do
+          render(text: (e || status).to_s, status: status)
+        end
       end
     end
 
-    :private
+    private
 
     def hostname
       "http://#{request.host}"
     end
-
-
   end
 end
--- app/models/qonsole_rails/qonsole_config.rb
+++ app/models/qonsole_rails/qonsole_config.rb
@@ -1,13 +1,14 @@
 module QonsoleRails
+  # Domain model to encapsulate a Qonsole configuration
   class QonsoleConfig
-    CONFIG_DIR = "config"
-    DEFAULT_CONFIG_FILE = "qonsole.json"
+    CONFIG_DIR = "config".freeze
+    DEFAULT_CONFIG_FILE = "qonsole.json".freeze
     DEFAULT_QUERY_TIMEOUT = 60
 
     attr_reader :config, :host
 
-    def initialize( params, host = nil  )
-      @config = qonsole_json.merge( params ).with_indifferent_access
+    def initialize(params, host = nil)
+      @config = qonsole_json.merge(params).with_indifferent_access
       @host = host
     end
 
@@ -44,15 +45,15 @@ module QonsoleRails
     end
 
     def absolute_endpoint
-      absolute_url( endpoint )
+      absolute_url(endpoint)
     end
 
     def valid_endpoint?
-      known_endpoint?( endpoint )
+      known_endpoint?(endpoint)
     end
 
-    def known_endpoint?( url )
-      endpoints.has_value?( url )
+    def known_endpoint?(url)
+      endpoints.value?(url)
     end
 
     def sparql_service_options
@@ -66,22 +67,21 @@ module QonsoleRails
       config[:query_timeout] || DEFAULT_QUERY_TIMEOUT
     end
 
-    :private
+    private
 
-    def qonsole_json( config_file_name = DEFAULT_CONFIG_FILE )
+    def qonsole_json(config_file_name = DEFAULT_CONFIG_FILE)
       unless defined? @@static_config
-        config = File.join( Rails.root, CONFIG_DIR, config_file_name )
-        raise "Missing qonsole configuration file config/#{config_file_name}" unless File.exist?( config )
+        config = File.join(Rails.root, CONFIG_DIR, config_file_name)
+        raise "Missing qonsole configuration file config/#{config_file_name}" unless File.exist?(config)
 
-        @@static_config = JSON.parse( IO.read( config ) )
+        @@static_config = JSON.parse(IO.read(config))
       end
 
       @@static_config
     end
 
-    def absolute_url( url )
-      url.start_with?( "http:" ) ? url : "#{host}#{url}"
+    def absolute_url(url)
+      url.start_with?("http:") ? url : "#{host}#{url}"
     end
-
   end
 end
